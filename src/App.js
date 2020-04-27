eaf39b56c906e32b42c560910de8c85dfffb5830
omar-albakri <albakrio@outlook.com>
2020-04-27 14:33:41 -0700

    adding header component and links to always appearon the top

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
-import ShopPage from './pages/shop/ShopPage'
-
+import ShopPage from './pages/shop/ShopPage';
+import Header from './components/header/Header';
 
 // const HatsPage = (props) => {
 // 	console.log(props);
-	
+
 // 	return (
 // 		<div>
 // 			<h1>Hats Page</h1>
@@ -18,6 +18,7 @@ import ShopPage from './pages/shop/ShopPage'
 function App() {
 	return (
 		<div className='App'>
+			<Header />
 			<Switch>
 				<Route component={HomePage} path='/' exact />
 				<Route component={ShopPage} path='/shop' />
